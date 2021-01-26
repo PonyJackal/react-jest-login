52433daf3b4a06c40cfc362ecaae973c671917e1
json <json1125@outlook.com>
2021-01-26 14:26:45 +0000

    add test: error messages should be removed if form is valid

--- src/components/login/Login.test.js
+++ src/components/login/Login.test.js
@@ -64,7 +64,7 @@ describe("check validation", () => {
         screen.getByText("Password must be more than 4 characters")
     })
 
-    it("Form Success", () => {
+    it("Forspanm Success", () => {
         const login = render(<Login />)
         const inputEmail = login.getByLabelText("Email")
         const inputPassword = login.getByLabelText("Password")
@@ -73,7 +73,33 @@ describe("check validation", () => {
         fireEvent.change(inputEmail, { target: { value: "a@a.com" } })
         fireEvent.change(inputPassword, { target: { value: "1234" } })
         fireEvent.click(submitButton)
-
+        //show submit text
         screen.getByText("Form submitted successfully")
+    })
+
+    it("Error messages should be removed if form is valid", () => {
+        const login = render(<Login />)
+        const inputEmail = login.getByLabelText("Email")
+        const inputPassword = login.getByLabelText("Password")
+        const submitButton = login.getByTestId('submit-button')
+
+        fireEvent.change(inputEmail, { target: { value: "a" } })
+        fireEvent.change(inputPassword, { target: { value: "1" } })
+        fireEvent.click(submitButton)
+
+        fireEvent.change(inputEmail, { target: { value: "a@a.com" } })
+        fireEvent.change(inputPassword, { target: { value: "1234" } })
+        fireEvent.click(submitButton)
+        //show submit text
+        screen.getByText("Form submitted successfully")
+        //error messages sbould not exist
+        const emailError1 = screen.queryByText("Email cannot be blank")
+        const emailError2 = screen.queryByText("Invalid email format")
+        const passwordError1 = screen.queryByText("Password cannot be blank")
+        const passwordError2 = screen.queryByText("Password must be more than 4 characters")
+        expect(emailError1).toBeNull()
+        expect(emailError2).toBeNull()
+        expect(passwordError1).toBeNull()
+        expect(passwordError2).toBeNull()
     })
 })
